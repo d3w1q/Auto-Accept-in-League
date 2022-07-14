645d1ced8cb6b63581ac9c0cdd3ec513256c1a5a
d3w1q <72358636+d3w1q@users.noreply.github.com>
2022-07-14 20:16:03 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class LoLAA
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoLAA));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkBox1.Size = new System.Drawing.Size(102, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Auto Accept";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // LoLAA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.Bez_nazwy_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(444, 238);
+            this.Controls.Add(this.checkBox1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoLAA";
+            this.Text = "LoLAA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoItX3Lib;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp1
+{
+    public partial class LoLAA : Form
+    {
+        IntPtr handle;
+        string WINDOW_NAME = "League of Legends";
+
+
+        AutoItX3 au3 = new AutoItX3();
+
+
+        public RECT rect;
+
+        public struct RECT
+        {
+            public int left, top, right, bottom;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string IpClassName, string IpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(IntPtr hwn, out RECT IpRect); 
+
+        int col;
+
+
+
+        public LoLAA()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread AA = new Thread(auto_accept) { IsBackground = true };
+            AA.Start();
+        }
+
+        private void auto_accept()
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    readwindow();
+                    col = au3.PixelGetColor(rect.left + 584, rect.top + 555);
+                    if (col == 0x1E252A)
+                    {
+                        Thread.Sleep(20);
+                        au3.MouseMove(rect.left + 584, rect.top + 555, 1);
+                        au3.MouseClick("LEFT");
+                        Thread.Sleep(20);
+                        au3.MouseClick("LEFT");
+                        Thread.Sleep(5000);
+                    }
+                }
+
+                Thread.Sleep(20);
+
+            }
+        }
+
+
+        void readwindow()
+        {
+            handle = FindWindow(null, WINDOW_NAME);
+            GetWindowRect(handle, out rect);
+        }
+
+
+
+
+
+    }
+
+
+
+}
